198c445c29d06aec5270ae4331ae4f5972a17e1a
Vandenborne Stefan <stefan@illutek.be>
2016-02-09 23:07:22 +0100

    Slider met slogans in de header onder het logo geplaatst

--- templates/layout/header.inc.php
+++ templates/layout/header.inc.php
@@ -9,14 +9,17 @@
 <header>
     <div class="container">
         <div class="row">
-            <div class="col-md-3 logo">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-                </a>
+            <div class="col-md-8">
+                <div class="col-md-12 logo">
+                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+                        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+                    </a>
+                </div>
+                <div class="col-md-12 slogans">
+                    <?php print render($page['slogan']); ?>
+                </div>
             </div>
-            <div class="col-md-5 slogans">
-                <?php print render($page['slogan']); ?>
-            </div>
+
             <div class="col-md-4 certification">
                 <img src="<?php print $imagepath; ?>/LRQA-2010-vca-petrochemie_white.png" height="100">
                 <img src="<?php print $imagepath; ?>/Bureau-Veritas-Certificaten-VCA-400x186.png" height="70">
