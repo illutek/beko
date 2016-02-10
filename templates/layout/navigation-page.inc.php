3ebb4af5df5f369c0acd9927186e4c8e212ad8a7
Vandenborne Stefan <stefan@illutek.be>
2016-02-10 22:54:03 +0100

    nav voor page.tpl.php aangepast

---
+++ templates/layout/navigation-page.inc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stefan
+ * Date: 5/01/2016
+ * Time: 15:03
+ */
+?>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav with-indicator">
+                <li class="nav-item">
+                    <a href="<?php print $front_page; ?>"><i class="fa fa-home"></i>Home</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
