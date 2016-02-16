f92910c72834460de5d3bb9ff5e1047ae8bd4ea4
Vandenborne Stefan <stefan@illutek.be>
2016-02-16 16:43:50 +0100

    modal regions ingesteld

--- templates/layout/page.tpl.php
+++ templates/layout/page.tpl.php
@@ -25,7 +25,7 @@ include 'header.inc.php';
             <div class="col-md-12 section-content wow fadeIn" data-wow-duration="3s">
                 <a id="main-content"></a>
                 <?php print render($title_prefix); ?>
-                <?php if ($title): ?><h1 class="title" id="page-title"><?php // print $title; ?></h1><?php endif; ?>
+                <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
                 <?php print render($title_suffix); ?>
                 <?php if ($tabs): ?>
                     <div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
@@ -60,37 +60,10 @@ bij Drupal 1 inhoudstype aanmaken (teksten-footer) met een view deze alle printe
 <div class="modal-container">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 col-md-offset-2 custom-modal">
+            <div class="col-md-6 col-md-offset-1 custom-modal">
                 <div class="close-button"><i class="fa fa-times"></i></div>
                 <div class="content-modal">
-                    <h3>Copyright</h3>
-
-                    <p>De inhoud van deze website (teksten, ideeën, foto’s …(geen exhaustieve lijst))
-                        is auteursrechtelijk beschermd. De auteursrechten berusten bij BEKO bvba, haar
-                        klanten en haar partners. Alle rechten zijn voorbehouden. Informatie van de
-                        website mag, tenzij anders is aangegeven, niet worden gekopieerd, overgeplaatst,
-                        verspreid of opgeslagen zonder onze schriftelijke toestemming vooraf.
-                        Wijzigingen op de inhoud van de site zijn verboden.
-                    </p>
-
-                    <h3>Algemene voorwaarden</h3>
-
-                    <p>De ontvangst van de factuur geldt van rechtswege en volgens art. 1139 van het
-                        burgerlijk Wetboek als ingebrekestelling zonder dat er enige akte nodig is en
-                        alleen door verstrijken van de termijn.</p>
-
-                    <p>Ingeval de betaling niet volgt binnen de acht dagen na het opzenden van een
-                        aangetekend schrijven is de schuldenaar gehouden bij toepassing van art. 1147
-                        van het burgerlijk wetboek tot betaling van een schadevergoeding wegens
-                        wanbetaling. Conventioneel en onverminderbaar vastgesteld op 12% met een
-                        minimum van 25 euro.
-                    </p>
-
-                    <p>Alle klachten dienen binnen de vijf dagen aangetekend te gebeuren.
-
-                    <p>
-
-                    <p>Voor alle betwistingen zijn enkel de Rechtbanken van Turnhout bevoegd.</p>
+                    <?php print render($page['copyconditions']); ?>
                 </div>
             </div>
         </div>
